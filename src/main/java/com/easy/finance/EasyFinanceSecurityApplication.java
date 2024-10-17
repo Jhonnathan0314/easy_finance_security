79e2725621f7b2c80d91cd09873f274a997f6897
Jhonnathan0314 <jffc314@gmail.com>
2024-10-16 21:50:21 -0500

    Empty project

---
+++ src/main/java/com/easy/finance/EasyFinanceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.easy.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFinanceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFinanceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy/finance/EasyFinanceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFinanceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
